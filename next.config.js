c93430aa2848310284c6db4b7811011053a5198d
David Randell <davidrandell@Davids-MacBook-Pro.local>
2025-04-22 14:42:17 +0100

    Optimize for production deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   images: {
     domains: ['upload.wikimedia.org', 'www.ryanair.com', 'www.easyjet.com'],
+    unoptimized: process.env.NODE_ENV === 'development',
+  },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
   },
 };
 
